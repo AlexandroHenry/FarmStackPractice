aae7edb639fbfdd795943a1cd2409a0064926e5d
Seungchul Ha <seungchulha@Seungchului-MacBookPro.local>
2023-04-05 15:46:36 +0900

    Finish Basic FrontEnd

--- BACKEND/database.py
+++ BACKEND/database.py
@@ -1,9 +1,7 @@
+import motor.motor_asyncio
 from model import Todo
 
-# MongoDB driver
-import motor.motor_asyncio
-
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
 database = client.TodoList
 collection = database.todo
 
@@ -23,14 +21,12 @@ async def create_todo(todo):
     result = await collection.insert_one(document)
     return document
 
-async def update_todo(title, desc):
-    await collection.update_one({"title": title}, {"$set": {
-        "description": desc
-    }})
 
+async def update_todo(title, desc):
+    await collection.update_one({"title": title}, {"$set": {"description": desc}})
     document = await collection.find_one({"title": title})
     return document
 
-async def delete_todo(title):
+async def remove_todo(title):
     await collection.delete_one({"title": title})
     return True
--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # FarmStackPractice
 
+
+# BACKEND (FastAPI)
 pip install "motor[srv]"
 
 ### Initial Setting
 1. requirements.txt
 2. pipenv shell -> Pipfile created
 3. pipenv install -r requirements.txt -> Pipfile.lock created
+
+# FRONTEND (REACT)
+1. NodeJS
+2. npx create-react-app frontend
+3. npm start
+4. npm install axios bootstrap
+
+Edited Files:
+index.js
+App.js
+src > Components > Todo.js & TodoListView.js
